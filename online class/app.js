333908a68ed2d5916a0b582f4bee5cc6a435560c
sarah sajid <123872994+sarah-sajid2002@users.noreply.github.com>
2023-04-07 01:49:59 +0500

    Add files via upload

---
+++ online class/app.js
@@ -0,0 +1,224 @@
+// java script online class
+
+// js variables:
+//2 ways of defining variable  var let
+
+// var a = 20;
+// //sdfjdfldlf
+// a =20;
+// var a =20;
+
+// var a =20;
+
+
+// let e =30;
+// //fddlg
+// e = 30;
+
+// ========================================================
+// camelCase
+// legal commands
+// special char  $ _
+// let myName = "sarah";
+
+// let avg_of_two_numbers = 20;  // snake case or underscore
+
+// let $avgOfTwoNumbers ;  // $ case
+
+// let myName1 = 20;
+
+// //  illegal command
+
+// // let my name = "sarah";
+
+// =================data types====================
+//string, boolean, no
+// let myName = "sarah"; //string
+// let MyName = "@"; //string
+
+// let myNo = -1; //no
+// myNo = -1000; //no
+// myNo = 56; //no
+
+//boolean (true false)
+// 2>3 flase
+
+// =================end of class 1=========================
+//concatenation string + string
+// let firstName = "sarah";
+// let secondName = "sajid";
+// console.log("sarah " + "sajid");
+// document.write("sarah " + "sajid")
+
+//concatenation string + no
+// let firstName = "sarah";
+// let myNo = "12";
+// console.log(firstName + " " + myNo);
+
+// let myNo1 = "12";
+// let myNo2 = "42";
+// console.log(myNo1 + myNo2);
+
+// myNo1 = "30";
+// myNo2 = "10";
+// console.log(myNo1 * myNo2); 
+
+// ============================includes====================
+// let firstName = "sarah";
+// console.log(firstName.includes("H"));
+
+// ================converrsion of string 3 methods:=============
+// let age = prompt("enter your age: ");
+// console.log(typeof age);
+
+// method 1
+// let age = prompt("enter your age: ");
+// console.log(typeof age);
+// age = parseInt(age);
+// console.log(typeof age);
+// // method 2
+// console.log(+age);
+// // method 3
+// console.log(Number.parseInt(age));
+
+// ========================operators=====================
+// + - * / % **
+
+// let a = 7;
+// let b = 2;
+
+// console.log("a+b " , a+b);
+// console.log("a-b " , a-b);
+// console.log("a*b " , a*b);
+// console.log("a/b " , a/b);
+// console.log("a%b " , a%b);
+// console.log("a**b " , a**b);
+
+// ===========================
+// ==(number), ===(type)
+// let a = 3;
+// let b = 4;
+// if (3 > 4){
+//     console.log("no")
+// }
+// else{
+//     console.log("yes")
+// }
+
+// ====================for loop
+// for(i=0; i<=10; i++ ){
+//     console.log(i);
+// }
+
+// ======================assignmnet1=========
+
+// q1
+
+// let num_1 = +prompt("enter no 1:");
+// let num_2 = +prompt("enter no 2:");
+
+// if(num_1 > num_2){
+//     console.log(num_1 + " is greater");
+// }
+// else if(num_2 > num_1){
+//     console.log(num_2 + " is greater");
+// }
+// else{
+//     console.log("both nos are equal");
+// }
+
+//q2
+
+// let num = +prompt("enter no:")
+// if (num > 0){
+//     console.log("+ sign")
+// }
+// else if (num < 0){
+//     console.log("- sign")
+// }
+// else if (num === 0){
+//     console.log("entered no is zero")
+// }
+ 
+
+// //q3
+// let num_1 = +prompt("enter no 1:");
+// let num_2 = +prompt("enter no 2:"); 
+// let num_3 = +prompt("enter no 3:"); 
+// let num_4 = +prompt("enter no 4:"); 
+// let num_5 = +prompt("enter no 5:");
+
+// if (num_1 > num_2 && num_1 > num_3 && num_1 > num_4 && num_1 > num_5){
+//     console.log(num_1 + " is greatest")
+// }
+// else if(num_2 > num_1 && num_2 > num_3 && num_2 > num_4 && num_2 > num_5){
+//     console.log(num_2 + " is greatest")
+// }
+// else if(num_3 > num_1 && num_3 > num_2 && num_3 > num_4 && num_3 > num_5){
+//     console.log(num_3 + " is greatest")
+// }
+// else if(num_4 > num_1 && num_4 > num_2 && num_4 > num_3 && num_4 > num_5){
+//     console.log(num_4 + " is greatest")
+// }
+// else if(num_5 > num_1 && num_5 > num_2 && num_5 > num_3 && num_5 > num_4){
+//     console.log(num_5 + " is greatest")
+// }
+// else{
+//     console.log("no number is greatest you have entered 2 equal numbers")
+// }
+
+  
+//q4
+
+// for (i=0; i<=15; i++){
+//     if (i % 2 == 0 ){
+//         console.log(i + ": is even")
+//     }
+//     else{
+//         console.log(i + ": is odd")
+//     }
+// }
+
+
+//q6
+// for (i=1; i<=100; i++){
+//     if(i % 3 == 0 && i % 5 == 0){
+//         console.log(i + " FizzBuzz");
+//     }
+//     else if (i % 3 == 0){
+//         console.log(i + " Fizz");
+//     }
+//     else if (i % 5 ==0){
+//         console.log(i + " Buzz");
+//     }
+// }
+
+
+//q7
+//nested for loop
+// for(i = 1; i<=5; i++){
+//     for(j = 1; j<=i; j++ ){
+//        document.write("*");
+//     }
+//     document.write("<br>");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
